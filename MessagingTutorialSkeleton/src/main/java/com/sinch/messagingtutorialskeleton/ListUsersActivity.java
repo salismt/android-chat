e0455919c18d5486c6de1e78619d81d219e92f70
Salis <tsalis.mohammad@gmail.com>
2015-07-05 04:51:14 +0700

    Initial commit

---
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/ListUsersActivity.java
@@ -0,0 +1,84 @@
+package com.sinch.messagingtutorialskeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.messagingtutorialskeleton.R;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+
+/**
+ * Created by mtsalis31 on 02-Jul-15.
+ */
+public class ListUsersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+
+        String currentUserId = ParseUser.getCurrentUser().getObjectId();
+        final ArrayList names = new ArrayList<String>();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        //don't include yourself
+        query.whereNotEqualTo("objectId", currentUserId);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> userList, ParseException e) {
+                if(e == null) {
+                    for(int i=0; i<userList.size(); i++ ){
+
+                        names.add(userList.get(1).getUsername().toString());
+
+                    }
+
+                    ListView usersListView = (ListView)findViewById(R.id.usersListView);
+                    ArrayAdapter namesArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.user_list_item, names);
+                    usersListView.setAdapter(namesArrayAdapter);
+
+                    usersListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                        @Override
+                        public void onItemClick(AdapterView<?> a, View v, int i, long l) {
+                            openConversation(names, i);
+                        }
+                    });
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Error loading user list", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+    }
+
+    public void openConversation(ArrayList<String> names, int pos) {
+        ParseQuery<ParseUser>  query = ParseUser.getQuery();
+        query.whereEqualTo("username", names.get(pos));
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> list, ParseException e) {
+                if(e == null){
+                    //start the messaging activity
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error finding that user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+}
---
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.sinch.messagingtutorialskeleton;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.*;
+import android.widget.Toast;
+
+import com.example.messagingtutorialskeleton.R;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "GNsyBu7UWQ8qRkj5Xg0LhAydJ4QIVFafD5s0EOz0", "2YAQJ9GmIKtg2YuxB5UhcjiRHSjr6CE7pS3cyfYw");
+
+
+        Button loginButton;
+        Button signUpButton;
+        final EditText usernameField;
+        final EditText passwordField;
+
+        // Initialize and connect the buttons with new variables
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signupButton);
+        usernameField = (EditText) findViewById(R.id.loginUsername);
+        passwordField = (EditText) findViewById(R.id.loginPassword);
+
+        // Initialize ListUsersActivity
+        final Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+        // Initialize Sinch Service
+        final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
+
+
+        //See if users are already log in
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            //start sinch service
+            startService(serviceIntent);
+            //start next activity
+            startActivity(intent);
+        }
+
+
+
+        // Set an onClickListener on login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+                // Check if the submitted user already exist
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    public void done(ParseUser user, com.parse.ParseException e) {
+                        if (user != null) {
+                            //start sinch service
+                            startService(serviceIntent);
+                            //start next activity
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "There was an error logging in.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+            // Set an onClickListener on signup button
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(com.parse.ParseException e) {
+                        if (e == null) {
+                            //start sinch service
+                            startService(serviceIntent);
+                            //start next activity
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "There was an error signing up."
+                                    , Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    public void onDestroy() {
+        stopService(new Intent(this, MessageService.class));
+        super.onDestroy();
+    }
+
+}
